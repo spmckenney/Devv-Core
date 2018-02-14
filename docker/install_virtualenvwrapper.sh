6a28e7d5ddbd1f76239cbf5b74a92997c6f51cee
Shawn McKenney <shawn.mckenney@emmion.com>
2018-02-14 01:26:52 -0700

    Change user to dcrunner

--- docker/install_virtualenvwrapper.sh
+++ docker/install_virtualenvwrapper.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-WORKON_HOME=/home/bcrunner/py_virtual_env
+WORKON_HOME=/home/dcrunner/py_virtual_env
 VIRTUALENVWRAPPER_PYTHON=`which python3`
 
 mkdir -p ${WORKON_HOME}
 source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv --system-site-packages bc-dev
+mkvirtualenv --system-site-packages dc-dev
 pip install --upgrade ipython
 
 # Bug in pip/scipy, numpy must be installed first
