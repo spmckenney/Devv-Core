97655610084c842b387b726e8bc4d48101950817
Shawn McKenney <shawn.mckenney@emmion.com>
2018-03-16 21:06:56 -0600

    DEV-94 Initial add of buffer test

---
+++ src/tests/queues/circular_buffer_test.cpp
@@ -0,0 +1,13 @@
+#include <memory>
+#include "queues/DevcashRingQueue.h"
+#include "types/DevcashMessage.h"
+
+struct BufferTester {
+};
+
+int
+main(int, char**) {
+
+  Devcash::DevcashRingQueue rq{};
+};
+
