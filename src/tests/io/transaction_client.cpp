c37f7fb2df438caabee813cf85e6f14697e8523a
Shawn McKenney <shawn.mckenney@emmion.com>
2018-03-26 00:15:23 -0700

    Clean up transaction tests

--- src/tests/io/transaction_client.cpp
+++ src/tests/io/transaction_client.cpp
@@ -7,8 +7,7 @@
 #include "io/constants.h"
 
 void print_devcash_message(Devcash::DevcashMessageUniquePtr message) {
-  LOG(info) << "Got a message";
-  LOG(info) << "DC->uri: " << message->uri;
+  LOG(info) << "DevcashMesage->uri: " << message->uri;
   return;
 }
 
@@ -19,63 +18,24 @@ int
 main(int argc, char** argv) {
 
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " uri" << std::endl;
-    std::cerr << "ex: " << argv[0] << " tcp://localhost:55557" << std::endl;
+    LOG(error) << "Usage: " << argv[0] << " endpoint";
+    LOG(error) << "ex: " << argv[0] << " tcp://localhost:55557";
     exit(-1);
   }
 
-  std::string host_uri = argv[1];
+  std::string endpoint = argv[1];
 
-  std::cout << "Connecting to " << host_uri << std::endl;
+  LOG(info) << "Connecting to " << endpoint;
 
-  /*
-
-  int opt;
-  po::options_description desc("Allowed options");
-  desc.add_options()
-    ("help", "produce help message")
-    ("optimization", po::value<int>(&opt)->default_value(10),
-     "optimization level")
-    ("include-path,I", po::value< std::vector<std::string> >(),
-     "include path")
-    ;
-
-     std::vector<std::thread> allThreads{};
-  */
   // Zmq Context
   zmq::context_t context(1);
 
   // start ZmqClient
   Devcash::io::TransactionClient client{context};
-  client.AddConnection(host_uri);
+  client.AddConnection(endpoint);
   client.AttachCallback(print_devcash_message);
 
   client.Run();
 
-  /*
-  std::thread clientThread([&client]() noexcept {
-    LOG(info) << "Starting Client thread ...";
-    client.Run();
-    LOG(info) << "Client stopped.";
-  });
-  */
-
-  /*
-  client.waitUntilRunning();
-  allThreads.emplace_back(std::move(clientThread));
-
-  LOG(info) << "Starting main event loop...";
-  mainEventLoop.run();
-  LOG(info) << "Main event loop got stopped";
-
-  client.stop();
-  client.waitUntilStopped();
-  */
-
-  /*
-  for (auto& t : allThreads) {
-    t.join();
-  }
-  */
   return 0;
 }
--- src/tests/io/transaction_server.cpp
+++ src/tests/io/transaction_server.cpp
@@ -10,14 +10,14 @@ int
 main(int argc, char** argv) {
 
   if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " bind-endpoint [URI]" << std::endl;
-    std::cerr << "ex: " << argv[0] << " tcp://*:55557 my-message" << std::endl;
+    LOG(error) << "Usage: " << argv[0] << " bind-endpoint [URI]";
+    LOG(error) << "ex: " << argv[0] << " tcp://*:55557 my-message";
     exit(-1);
   }
 
-  std::string host_uri = argv[1];
+  std::string bind_endpoint = argv[1];
 
-  std::cout << "Binding to " << host_uri << std::endl;
+  LOG(info) << "Binding to " << bind_endpoint;
 
   std::string message;
   if (argc > 2) {
@@ -29,49 +29,18 @@ main(int argc, char** argv) {
   // Zmq Context
   zmq::context_t context(1);
 
-  Devcash::io::TransactionServer server{context, host_uri};
-
-  /*
-  std::thread serverThread([&server]() noexcept {
-    LOG(info) << "Starting Server thread ...";
-    server.run();
-    LOG(info) << "Server stopped.";
-  });
-
-  server.waitUntilRunning();
-  allThreads.emplace_back(std::move(serverThread));
-
-  LOG(info) << "Creating Event Thread";;
-  std::thread event_loop_thread([&mainEventLoop, &server]() noexcept {
-      LOG(info) << "Starting main event loop...";
-      mainEventLoop.run();
-      LOG(info) << "Main event loop got stopped";
-
-      server.stop();
-      server.waitUntilStopped();
-    });
-  LOG(info) << "Creating Event Thread";;
-
-  allThreads.emplace_back(std::move(event_loop_thread));
-  */
+  Devcash::io::TransactionServer server{context, bind_endpoint};
 
   server.StartServer();
-
-  LOG(info) << "Gonna sleep(1)";
 
   for (;;) {
     sleep(1);
     auto devcash_message = Devcash::DevcashMessageUniquePtr(new Devcash::DevcashMessage);
     devcash_message->uri = message;
     devcash_message->message_type = Devcash::eMessageType::VALID;
-    LOG(info) << "Sending message";
+    LOG(info) << "Sending message: " << message;
     server.QueueMessage(std::move(devcash_message));
   }
 
-  /*
-  for (auto& t : allThreads) {
-    t.join();
-  }
-  */
   return 0;
 }
