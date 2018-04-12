dee81a1234d876093344319cc4ee713574435988
Andree Jacobson <andree@jc-hpc.com>
2018-04-12 09:44:21 -0600

    Updated buildscript and CI pipeline to use slurm to build devcash on any node

--- scripts/build-devcash.sh
+++ scripts/build-devcash.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
-# Build script for devcash, assumes the executing party is cd'd into
-# the <root repository path>/scripts before running this command. This is
-# required because we don't know which absolute path the script will reside in.
-# -Andree, 4/11/2018
+# Build script for devcash, intended to be launched through SLURM via
+#   salloc -N 1 <path to script>/build-devcash.sh <branch>
 
+# Name of devcash repository to check out
 repo=devcash-core
+
+# Find root directory of the script location
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Read current container version from config file
 container_version=`cat ../.container_version`
 
-# Get a node to build on
-salloc -N 1
+# Find hostname we're going to build on
+build_host=$SLURM_NODELIST
 
+# And the number of cores / threads on that node (as SLURM reports)
+build_cores=$SLURM_TASKS_PER_NODE
+
+# Grab the branch name from the command line
+build_branch=$1
+
+# While still on master node, pull repo and checkout correct branch
+# (passed as argument to script)
 echo "Pulling repo"
-mkdir -p ~/Devel/${SLURM_NODELIST}
-cd ~/Devel/${SLURM_NODELIST}
+mkdir -p ~/Devel/$build_host
+cd ~/Devel/$build_host
 if [ -d $repo ]; then
   echo "Found repo, pulling changes"
   pushd $repo
 else
   echo "Repo nonexistent, cloning"
-  git clone git@bitbucket.org:leveragerock/devcash-core.git
+  git clone git@bitbucket.org:leveragerock/${repo}.git
 fi
-git checkout $1
+git checkout $build_branch
 git pull
 cd ../..
 
 docker_container=x86_64-ubuntu16.04-devcash-v${container_version}
 
 # Unpack the tarball for the build image onto the ramdisk (using this to build)
-ssh $SLURM_NODELIST /opt/local/bin/ch-tar2dir /z/c-cloud/tars/${docker_container}.tar.gz /mnt/ramdisk
+ssh $build_host \
+  /opt/local/bin/ch-tar2dir /z/c-cloud/tars/${docker_container}.tar.gz /mnt/ramdisk
 
-# Now, run the build script
-ssh $SLURM_NODELIST /opt/local/bin/ch-run /mnt/ramdisk/${docker_container} -- sh -c "cd /home/bitbucket/Devel/${SLURM_NODELIST}/devcash-core/src; mkdir -p build; cd build; cmake ..; make -j 16"
-
-exit
-
+# Now, run the build script 
+ssh $build_host \
+  /opt/local/bin/ch-run /mnt/ramdisk/${docker_container} -- \
+  sh -c "cd ~/Devel/${build_host}/${repo}/src; mkdir -p build; cd build; cmake ..; make -j $build_cores"
