103cc1390d7f1baa4ba6a5e6a5403b6c146fcd52
Shawn McKenney <shawn.mckenney@emmion.com>
2018-07-18 09:11:38 -0500

    Changed unsigned int tx-limit -> uint64_t tx-amount Updated mixed-type math to convert to int64_t

--- src/turbulent.cpp
+++ src/turbulent.cpp
@@ -2,7 +2,7 @@
  * turbulent.cpp
  * Creates generate_count transactions as follows:
  * 1.  INN transactions create coins for every address
- * 2.  Each peer address attempts to send a random number of coins up to tx_limit
+ * 2.  Each peer address attempts to send a random number of coins up to tx_amount
  *     to a random address other than itself
  * 3.  Many transactions will be invalid, but valid transactions should also appear indefinitely
  *
@@ -33,7 +33,7 @@ struct turbulent_options {
   std::string wallet_keys;
   std::string key_pass;
   unsigned int generate_count;
-  unsigned int tx_limit;
+  uint64_t tx_amount;
   eDebugMode debug_mode;
 };
 
@@ -72,15 +72,15 @@ int main(int argc, char* argv[]) {
 
     std::vector<Transfer> xfers;
     Transfer inn_transfer(inn_addr, 0
-      , -1*addr_count*(addr_count-1)*options->tx_limit, 0);
+      , -1l*addr_count*(addr_count-1)*options->tx_amount, 0);
     xfers.push_back(inn_transfer);
     for (size_t i = 0; i < addr_count; ++i) {
       Transfer transfer(keys.getWalletAddr(i), 0
-        , (addr_count-1)*options->tx_limit, 0);
+        , (addr_count-1)*options->tx_amount, 0);
       xfers.push_back(transfer);
     }
     uint64_t nonce = GetMillisecondsSinceEpoch() + (1000000
-                     * (options->node_index + 1) * (options->tx_limit + 1));
+                     * (options->node_index + 1) * (options->tx_amount + 1));
 	std::vector<byte> nonce_bin;
     Uint64ToBin(nonce, nonce_bin);
     Tier2Transaction inn_tx(eOpType::Create, xfers, nonce_bin,
@@ -93,10 +93,10 @@ int main(int argc, char* argv[]) {
     while (counter < options->generate_count) {
         for (size_t i = 0; i < addr_count; ++i) {
           size_t j = std::rand() % addr_count;
-          size_t amount = std::rand() % options->tx_limit;
+          size_t amount = std::rand() % options->tx_amount;
           if (i == j) { continue; }
           std::vector<Transfer> peer_xfers;
-          Transfer sender(keys.getWalletAddr(i), 0, amount * -1, 0);
+          Transfer sender(keys.getWalletAddr(i), 0, -1l * amount, 0);
           peer_xfers.push_back(sender);
           Transfer receiver(keys.getWalletAddr(j), 0, amount, 0);
           peer_xfers.push_back(receiver);
@@ -153,7 +153,7 @@ std::unique_ptr<struct turbulent_options> ParseTurbulentOptions(int argc, char**
 \n\
 Creates generate_count transactions as follows:\n\
 1.  INN transactions create coins for every address\n\
-2.  Each peer address attempts to send a random number of coins up to tx_limit\n\
+2.  Each peer address attempts to send a random number of coins up to tx_amount\n\
     to a random address other than itself\n\
 3.  Many transactions will be invalid, but valid transactions should also appear indefinitely\n\
 \n\
@@ -168,7 +168,7 @@ Required parameters");
         ("wallet-keys", po::value<std::string>(), "Path to Wallet key file")
         ("key-pass", po::value<std::string>(), "Password for private keys")
         ("generate-tx", po::value<unsigned int>(), "Generate at least this many Transactions")
-        ("tx-limit", po::value<unsigned int>(), "Number of transaction to process before shutting down.")
+        ("tx-amount", po::value<uint64_t>(), "Number of coins to transfer in transaction")
         ;
 
     po::options_description d2("Optional parameters");
@@ -276,12 +276,12 @@ Required parameters");
       options->generate_count = 0;
     }
 
-    if (vm.count("tx-limit")) {
-      options->tx_limit = vm["tx-limit"].as<unsigned int>();
-      LOG_INFO << "Transaction limit: " << options->tx_limit;
+    if (vm.count("tx-amount")) {
+      options->tx_amount = vm["tx-amount"].as<uint64_t>();
+      LOG_INFO << "Transaction amount: " << options->tx_amount;
     } else {
-      LOG_INFO << "Transaction limit was not set, defaulting to 0 (unlimited)";
-      options->tx_limit = 100;
+      options->tx_amount = 3210123;
+      LOG_INFO << "Transaction amount was not set, defaulting to " << options->tx_amount;
     }
   }
   catch (std::exception& e) {
