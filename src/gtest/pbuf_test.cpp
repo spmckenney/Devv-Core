cd575fd8f2bd8a28194775349140a58db16a1873
Shawn McKenney <shawn.mckenney@emmion.com>
2018-08-21 07:09:59 -0500

    Transaction tests

--- src/gtest/pbuf_test.cpp
+++ src/gtest/pbuf_test.cpp
@@ -56,6 +56,43 @@ class PbufTransactionTest : public ::testing::Test {
   void SetUp() override {
     // Code here will be called immediately after the constructor (right
     // before each test).
+    pb_transaction_0_.set_operation(Devv::proto::eOpType::OP_CREATE);
+    pb_transaction_0_.set_nonce(Bin2Str(Hex2Bin("86525F0665010000")));
+    pb_transaction_0_.set_sig(Bin2Str(Hex2Bin("69306402304BCFD89AE647DACA5A5D64FCEC0F66C66497F03449E8C6EEC239B1F94C1E5FC860CE5C37BBFC3142A4FFF2C857A8E55C023075918475133C249446114AC31D5DBD62AD74C254EDF1C9652D547CE906EF68504390ABC4724ADFB000B1C61454E871CC000000")));
+
+    std::vector<Transfer> transfer_vector;
+    auto transfer = Transfer("310272B05D9A8CF6E1565B965A5CCE6FF88ABD0C250BC17AB23745D512095C2AFCDB3640A2CBA7665F0FAADC26B96E8B8A9D"
+        , 0
+        , -6
+        , 0);
+    transfer_vector.push_back(transfer);
+
+    transfer = Transfer("2102E14466DC0E5A3E6EBBEAB5DD24ABE950E44EF2BEB509A5FD113460414A6EFAB4"
+        , 0
+        , 2
+        , 0);
+    transfer_vector.push_back(transfer);
+
+    transfer = Transfer("2102C85725EE136128BC7D9D609C0DD5B7370A8A02AB6F623BBFD504C6C0FF5D9368"
+        , 0
+        , 2
+        , 0);
+    transfer_vector.push_back(transfer);
+
+    transfer = Transfer("21030DD418BF0527D42251DF0254A139084611DFC6A0417CCE10550857DB7B59A3F6"
+        , 0
+        , 2
+        , 0);
+    transfer_vector.push_back(transfer);
+
+    for (auto& xfer: transfer_vector) {
+      //std::cout << xfer.getJSON() << std::endl;
+      auto pb_transfer = pb_transaction_0_.add_xfers();
+      pb_transfer->set_address(Bin2Str(xfer.getAddress().getCanonical()));
+      pb_transfer->set_coin(xfer.getCoin());
+      pb_transfer->set_amount(xfer.getAmount());
+      pb_transfer->set_delay(xfer.getDelay());
+    }
   }
 
   void TearDown() override {
@@ -72,12 +109,13 @@ class PbufTransactionTest : public ::testing::Test {
 
   // keys
   Devcash::KeyRing keys_;
+
+  Devv::proto::Transaction pb_transaction_0_;
 };
 
 TEST_F(PbufTransactionTest, defaultConstructor) {
   Devv::proto::Transaction pb_tx;
 
-  EXPECT_EQ(pb_tx.nonce_size(), 0);
   EXPECT_EQ(pb_tx.operation(), Devv::proto::eOpType::OP_CREATE);
 }
 
@@ -143,7 +181,57 @@ TEST_F(PbufTransactionTest, createTransaction_0) {
   }
 
   auto transaction = Devcash::CreateTransaction(pb_transaction, keys_);
+
+  EXPECT_EQ(transaction->getTransfers().size(), 4);
 }
+
+TEST_F(PbufTransactionTest, createTransaction_1) {
+  auto nonce_string = Bin2Str(Hex2Bin("86525F0665010000"));
+  EXPECT_EQ(pb_transaction_0_.nonce(), nonce_string);
+  EXPECT_EQ(pb_transaction_0_.xfers_size(), 4);
+}
+
+TEST_F(PbufTransactionTest, createTransaction_2) {
+  auto transaction_0 = Devcash::CreateTransaction(pb_transaction_0_, keys_);
+  auto transaction_1 = Devcash::CreateTransaction(pb_transaction_0_, keys_, true);
+
+  EXPECT_EQ(transaction_1->getTransfers().size(), 4);
+}
+
+TEST_F(PbufTransactionTest, createTransaction_empty_sig) {
+  pb_transaction_0_.set_sig("");
+  try {
+    auto transaction_0 = Devcash::CreateTransaction(pb_transaction_0_, keys_);
+    FAIL() << "Expected std::runtime_error";
+  }
+  catch (std::runtime_error const& err) {
+    EXPECT_EQ(err.what(), std::string("Invalid Signature size: 0"));
+  }
+  catch (...) {
+    FAIL() << "Expected std::runtime_error";
+  }
+}
+
+TEST_F(PbufTransactionTest, createTransaction_sig_wrong_size) {
+  pb_transaction_0_.set_sig("abc123");
+  try {
+    auto transaction_0 = Devcash::CreateTransaction(pb_transaction_0_, keys_);
+    FAIL() << "Expected std::runtime_error";
+  }
+  catch (std::runtime_error const& err) {
+    EXPECT_EQ(err.what(), std::string("Invalid Signature size: 6"));
+  }
+  catch (...) {
+    FAIL() << "Expected std::runtime_error";
+  }
+}
+
+TEST_F(PbufTransactionTest, createTransaction_bad_sig) {
+  pb_transaction_0_.set_sig(Bin2Str(Hex2Bin("deadbeef000BCFD89AE647DACA5A5D64FCEC0F66C66497F03449E8C6EEC239B1F94C1E5FC860CE5C37BBFC3142A4FFF2C857A8E55C023075918475133C249446114AC31D5DBD62AD74C254EDF1C9652D547CE906EF68504390ABC4724ADFB000B1C61454E871CC000000")));
+  auto transaction_0 = Devcash::CreateTransaction(pb_transaction_0_, keys_);
+}
+
+
 
 } // unnamed namespace
 
