8377204db9afe3f808f2cfed2aec8ceb120c7ed1
Shawn McKenney <shawn.mckenney@emmion.com>
2018-09-18 17:44:57 -0500

    Need sudo to usermod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,8 +7,11 @@
 H_USER_ID=${USER_ID:-1002}
 H_GROUP_ID=${GROUP_ID:-1002}
 
-usermod -u $H_USER_ID dcrunner
-chown -R $H_USER_ID:$H_GROUP_ID /opt/dcrunner
+echo "Changing ownership of /opt/dcrunner"
+chown_cmd="chown -R $H_USER_ID:$H_GROUP_ID /opt/dcrunner"
+
+sudo usermod -u $H_USER_ID dcrunner
+sudo ${chown_cmd}
 echo "Starting with UID:GID : $H_USER_ID:$H_GROUP_ID"
 
 if [ "$#" -eq 0 ]; then
