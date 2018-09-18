afb6045a0df946270e6ee73a73286dadd33e2f4f
Shawn McKenney <shawn.mckenney@emmion.com>
2018-09-18 17:53:13 -0500

    Fix typo

--- scripts/build-container-image.sh
+++ scripts/build-container-image.sh
@@ -5,7 +5,7 @@ arch=`uname -m`
 image_name="devvio-core-$arch-ubuntu16.04-dev"
 build="docker build -t ${image_name}:$ver -f Dockerfile-$arch-ubuntu16.04 ."
 dtoch="ch-docker2tar ${image_name}:${ver} /z/c-cloud/tars/"
-move="mv /z/c-cloud/tars/${image_name}:${ver}.tar.gz /z/c-cloud/tars/${$image_name}-${ver}.tar.gz"
+move="mv /z/c-cloud/tars/${image_name}:${ver}.tar.gz /z/c-cloud/tars/${image_name}-${ver}.tar.gz"
 chtodir="ch-tar2dir /z/c-cloud/tars/${image_name}-${ver}.tar.gz /z/c-cloud/dirs/"
 
 echo $build
