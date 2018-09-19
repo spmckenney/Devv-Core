c335cd5fa31a1df312a0fc7be8e6b37411a14282
Shawn McKenney <shawn.mckenney@emmion.com>
2018-09-18 18:01:42 -0600

    Added repo and release type to command line

--- scripts/build-container-image.sh
+++ scripts/build-container-image.sh
@@ -1,9 +1,30 @@
 #!/bin/bash -ex
 
 ver=$1
+repo=$2
+rel=$3
+
+if [ "${repo}X" = "devvio-coreX" ]; then
+    echo "repo: ${repo}"
+elif [ "${repo}X" = "devvio-innX" ]; then
+    echo "repo: ${repo}"
+else
+    echo "Unsupported repository"
+    exit -1
+fi
+
+if [ "${rel}X" = "prodX" ]; then
+    echo "rel: ${rel}"
+elif [ "${rel}X" = "devX" ]; then
+    echo "rel: ${rel}"
+else
+    echo "Unsupported release type (prod or dev)"
+    exit -1
+fi
+
 arch=`uname -m`
-image_name="devvio-core-$arch-ubuntu16.04-dev"
-build="docker build -t ${image_name}:$ver -f Dockerfile-$arch-ubuntu16.04 ."
+image_name="${repo}-$arch-ubuntu16.04-${rel}"
+build="docker build -t ${image_name}:$ver -f Dockerfile-$arch-ubuntu16.04-${rel} ."
 dtoch="ch-docker2tar ${image_name}:${ver} /z/c-cloud/tars/"
 move="mv /z/c-cloud/tars/${image_name}:${ver}.tar.gz /z/c-cloud/tars/${image_name}-${ver}.tar.gz"
 chtodir="ch-tar2dir /z/c-cloud/tars/${image_name}-${ver}.tar.gz /z/c-cloud/dirs/"
