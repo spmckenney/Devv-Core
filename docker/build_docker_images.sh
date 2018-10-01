d46b122a070d52d61a41499f784961cd03dbe2df
Shawn McKenney <shawn.mckenney@emmion.com>
2018-10-01 01:01:14 -0500

    Remove home directory variable from Dockerfiles

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -9,7 +9,7 @@ declare -a image_types=("dev" "prod")
 for image_type in "${image_types[@]}"
 do
     docker build \
-	   --build-arg VERSION=${ver} \
+	   --build-arg version=${ver} \
 	   -t devvio-x86_64-ubuntu16.04-${image_type}:${ver} \
 	   -f Dockerfile-x86_64-ubuntu16.04-${image_type} .
 done
