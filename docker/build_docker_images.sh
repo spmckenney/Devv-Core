92fc9aca19fbae00b9cd232b92841da28337853b
Shawn McKenney <shawn.mckenney@emmion.com>
2018-10-02 23:42:55 -0500

    Update install script and branch name

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 ver=$1
-branch=$2
+git_branch=$2
 
 ## Build dev and prod
 declare -a image_types=("dev" "prod")
@@ -11,7 +11,7 @@ for image_type in "${image_types[@]}"
 do
     docker build \
 	   --build-arg version=${ver} \
-	   --build-arg branch=${branch} \
+	   --build-arg branch=${git_branch} \
 	   -t devvio-x86_64-ubuntu16.04-${image_type}:${ver} \
 	   -f Dockerfile-x86_64-ubuntu16.04-${image_type} .
 done
