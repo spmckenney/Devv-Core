70b52b45ddc5f798b848c59b5f4f5290f5831243
Nick <nick@cloudsolar.co>
2018-10-08 15:23:47 -0700

    cleanup CoinRequest oracle

--- src/pbuf/devv_pbuf.h
+++ src/pbuf/devv_pbuf.h
@@ -173,7 +173,7 @@ std::vector<TransactionPtr> DecomposeProposal(const Devv::proto::Proposal& propo
                                              , const KeyRing& keys) {
   std::vector<TransactionPtr> ptrs;
   std::string oracle_name = proposal.oraclename();
-  if (oracle_name == coin_request::getOracleName()) {
+  if (oracle_name == CoinRequest::getOracleName()) {
     CoinRequest oracle(proposal.data());
     std::vector<TransactionPtr> actions = validateOracle(oracle, chain, keys);
     ptrs.insert(ptrs.end(), std::make_move_iterator(actions.begin()), std::make_move_iterator(actions.end()));
